a737d354664627fd103fd6245b8a0cf5085ab2b2
syed <syedb@bppcare.com.au>
2018-08-03 20:16:05 +1000

    created UI to display Race summary

--- DemoRaceApp/DemoRaceApp/ClientApp/src/app/app.module.ts
+++ DemoRaceApp/DemoRaceApp/ClientApp/src/app/app.module.ts
@@ -6,29 +6,26 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { DemoService } from './shared/demoService';
+import { RaceSummaryComponent} from './raceSummary/raceSummary.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    RaceSummaryComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: RaceSummaryComponent, pathMatch: 'full' },
+      { path: 'race-summary', component: RaceSummaryComponent }
     ])
   ],
-  providers: [],
+  providers: [DemoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
